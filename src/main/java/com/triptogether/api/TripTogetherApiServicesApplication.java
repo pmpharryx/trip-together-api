4d80c1e71d1e2a1139df71d2f0ff3c01ec2e995a
Phumiphat Tatiyawongsoonthorn <harryphat42@gmail.com>
2024-05-01 22:44:11 +0700

    chore: add Spring Boot initial project setup

---
+++ src/main/java/com/triptogether/api/TripTogetherApiServicesApplication.java
@@ -0,0 +1,13 @@
+package com.triptogether.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripTogetherApiServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripTogetherApiServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/triptogether/api/TripTogetherApiServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triptogether.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripTogetherApiServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
