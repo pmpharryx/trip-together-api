2ee72f3e31af2958079ef80539d5979add679e45
Kantawit Chatdamrongmongkol <ka1n11@hotmail.com>
2024-05-23 22:28:00 +0700

    feat(auth): add change password dto

---
+++ src/main/java/com/triptogether/api/auth/dto/ChangePasswordResponse.java
@@ -0,0 +1,14 @@
+package com.triptogether.api.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChangePasswordResponse {
+
+    private String statusCode;
+    private String statusMessage;
+}
