be6e4adbc02ad6ca88a3ab89e2c4ea3895572d65
Kantawit Chatdamrongmongkol <ka1n11@hotmail.com>
2024-05-29 23:06:22 +0700

    feat(auth): add base response builder to all services

--- src/main/java/com/triptogether/api/auth/service/AuthService.java
+++ src/main/java/com/triptogether/api/auth/service/AuthService.java
@@ -1,21 +1,39 @@
 package com.triptogether.api.auth.service;
 
 import com.triptogether.api.auth.dto.*;
+import com.triptogether.api.common.dto.ResponseData;
+import com.triptogether.api.common.model.User;
+import com.triptogether.api.auth.repository.AuthRepository;
+import com.triptogether.api.common.dto.Response;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class AuthService {
 
-    public SignUpResponse signUp(SignUpRequest request) {
-        return new SignUpResponse();
+    @Autowired
+    private AuthRepository authRepository;
+
+    public Response signUp(SignUpRequest request) {
+
+        authRepository.save(new User(request.getUsername(), request.getPassword(), request.getMobileNo()));
+        return Response.responseBuilder()
+                .statusMessage("Sign up successfully.")
+                .build();
     }
 
-    public SignInResponse signIn(SignInRequest request){
-        return new SignInResponse();
+    public ResponseData<SignInResponse> signIn(SignInRequest request){
+        return ResponseData.<SignInResponse>responseDataBuilder()
+                .statusMessage("Sign in successfully.")
+                .data(new SignInResponse())
+                .build();
     }
 
-    public ChangePasswordResponse changePassword(ChangePasswordRequest request){
-        return new ChangePasswordResponse();
+    public Response changePassword(ChangePasswordRequest request) {
+        return Response.responseBuilder()
+                .statusMessage("Changing password successfully.")
+                .build();
     }
 
 }
