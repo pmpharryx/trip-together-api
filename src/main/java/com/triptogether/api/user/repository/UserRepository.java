380e19f3ffcbd7202c1718d063401055722a8c95
Kantawit Chatdamrongmongkol <ka1n11@hotmail.com>
2024-05-29 22:40:34 +0700

    feat(user): add user repository

---
+++ src/main/java/com/triptogether/api/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.triptogether.api.user.repository;
+
+import com.triptogether.api.common.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+}
