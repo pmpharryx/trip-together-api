4dfaae624b11f0db0bac3597eac26cc1b2f7d3d8
Kantawit Chatdamrongmongkol <ka1n11@hotmail.com>
2024-07-04 21:43:46 +0700

    feat(auth): add custom query methods in auth repository

--- src/main/java/com/triptogether/api/auth/repository/AuthRepository.java
+++ src/main/java/com/triptogether/api/auth/repository/AuthRepository.java
@@ -1,9 +1,21 @@
 package com.triptogether.api.auth.repository;
 
 import com.triptogether.api.common.model.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 import java.util.UUID;
 
-public interface AuthRepository extends JpaRepository<User, UUID> {
 
+public interface AuthRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByMobileNo(String mobileNo);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE User user SET user.password = :newPassword WHERE user.userId = :userId")
+    void updatePassword(UUID userId, String newPassword);
 }
