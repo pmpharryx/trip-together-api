c4433ede327e2c2e5d54bc3fdf1e6d1bcee8b7b5
Kantawit Chatdamrongmongkol <ka1n11@hotmail.com>
2024-07-04 21:22:13 +0700

    fix(Common): refactor common response dto

--- src/main/java/com/triptogether/api/common/dto/ResponseDTO.java
+++ src/main/java/com/triptogether/api/common/dto/ResponseDTO.java
@@ -4,15 +4,21 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.experimental.SuperBuilder;
 
+import java.util.Map;
+
 @Data
 @SuperBuilder(toBuilder = true, builderMethodName = "responseBuilder")
-public class Response {
+public class ResponseDTO<T> {
 
     @Builder.Default
     private final String statusCode = "200";
 
     @Builder.Default
     private final String statusMessage = "Success";
+
+    private T data;
+
+    private Map<String,String> errors;
 
     @Override
     public String toString() {
