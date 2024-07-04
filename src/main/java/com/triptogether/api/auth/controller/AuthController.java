126736f5a0e1418b0f22bf0913da546204807377
Kantawit Chatdamrongmongkol <ka1n11@hotmail.com>
2024-07-04 21:47:00 +0700

    refactor(auth): refactor auth controller and service

--- src/main/java/com/triptogether/api/auth/controller/AuthController.java
+++ src/main/java/com/triptogether/api/auth/controller/AuthController.java
@@ -4,15 +4,21 @@ import com.triptogether.api.auth.dto.ChangePasswordRequest;
 import com.triptogether.api.auth.dto.SignInRequest;
 import com.triptogether.api.auth.dto.SignInResponse;
 import com.triptogether.api.auth.dto.SignUpRequest;
+import com.triptogether.api.auth.exception.ChangePasswordErrorException;
 import com.triptogether.api.auth.service.AuthService;
-import com.triptogether.api.common.dto.Response;
-import com.triptogether.api.common.dto.ResponseData;
+import com.triptogether.api.common.dto.ResponseDTO;
+import com.triptogether.api.auth.exception.MissingTokenException;
+import com.triptogether.api.auth.exception.SignInException;
+import com.triptogether.api.auth.exception.SignUpException;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/v1/auth")
@@ -22,20 +28,52 @@ public class AuthController {
     private AuthService authService;
 
     @PostMapping("/sign-up")
-    public ResponseEntity<Response> signUp(@RequestBody SignUpRequest request) {
-        Response response = authService.signUp(request);
+    public ResponseEntity<?> signUp(@Valid @RequestBody SignUpRequest request, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()){
+            Map<String, String> errors = new HashMap<>();
+            for (FieldError error : bindingResult.getFieldErrors()){
+                errors.put(error.getField(),error.getDefaultMessage());
+            }
+            throw new SignUpException("Sign-Up Error Exception",errors);
+        }
+        ResponseDTO<?> response = authService.signUp(request);
         return ResponseEntity.ok(response);
     }
 
     @PostMapping("/sign-in")
-    public ResponseEntity<ResponseData<SignInResponse>> signIn(@RequestBody SignInRequest request) {
-        ResponseData<SignInResponse> response = authService.signIn(request);
+    public ResponseEntity<ResponseDTO<SignInResponse>> signIn(@Valid @RequestBody SignInRequest request,
+                                                              BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            Map<String, String> errors = new HashMap<>();
+            for (FieldError error : bindingResult.getFieldErrors()) {
+                errors.put(error.getField(), error.getDefaultMessage());
+            }
+            throw new SignInException("Sign-In Error Exception", errors);
+        }
+        ResponseDTO<SignInResponse> response = authService.signIn(request);
         return ResponseEntity.ok(response);
     }
 
     @PostMapping("/change-password")
-    public ResponseEntity<Response> changePassword(@RequestBody ChangePasswordRequest request) {
-        Response response = authService.changePassword(request);
+    public ResponseEntity<ResponseDTO<?>> changePassword(@Valid @RequestBody ChangePasswordRequest request,
+                                                         @RequestHeader(value = "Authorization", required = false)
+                                                         String authorizationHeader, BindingResult bindingResult) {
+
+        if(bindingResult.hasErrors()) {
+            Map<String, String> errors = new HashMap<>();
+            for (FieldError error : bindingResult.getFieldErrors()) {
+                errors.put(error.getField(), error.getDefaultMessage());
+            }
+            throw new ChangePasswordErrorException("Change Password Error Exception", errors);
+        }
+
+        if(authorizationHeader == null){
+            Map<String, String> errors = new HashMap<>();
+            errors.put("authorizationHeader", "Authorization token is missing.");
+            throw new MissingTokenException("Missing Token Exception", errors);
+        }
+
+        ResponseDTO<?> response = authService.changePassword(request);
         return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/triptogether/api/auth/service/AuthService.java
+++ src/main/java/com/triptogether/api/auth/service/AuthService.java
@@ -1,12 +1,23 @@
 package com.triptogether.api.auth.service;
 
-import com.triptogether.api.auth.dto.*;
-import com.triptogether.api.common.dto.ResponseData;
-import com.triptogether.api.common.model.User;
+import com.triptogether.api.auth.dto.ChangePasswordRequest;
+import com.triptogether.api.auth.dto.SignInRequest;
+import com.triptogether.api.auth.dto.SignInResponse;
+import com.triptogether.api.auth.dto.SignUpRequest;
+import com.triptogether.api.auth.exception.ChangePasswordErrorException;
 import com.triptogether.api.auth.repository.AuthRepository;
-import com.triptogether.api.common.dto.Response;
+import com.triptogether.api.auth.utility.JwtUtils;
+import com.triptogether.api.common.dto.ResponseDTO;
+import com.triptogether.api.auth.exception.InvalidSignInDataException;
+import com.triptogether.api.auth.exception.SignUpDatabaseConstrainViolationException;
+import com.triptogether.api.common.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+import static com.triptogether.api.auth.utility.JwtUtils.createToken;
+import static com.triptogether.api.auth.utility.JwtUtils.getUserId;
 
 
 @Service
@@ -15,25 +26,86 @@ public class AuthService {
     @Autowired
     private AuthRepository authRepository;
 
-    public Response signUp(SignUpRequest request) {
+    public ResponseDTO<?> signUp(SignUpRequest request) {
 
+        // Pre-check for existing username
+        Optional<User> existingUserByUsername = authRepository.findByUsername(request.getUsername());
+        if(existingUserByUsername.isPresent()){
+            Map<String, String> errors = new HashMap<>();
+            errors.put("username", "Username already exists");
+            throw new SignUpDatabaseConstrainViolationException("Sign-up database constraint violation", errors);
+        }
+
+        // Pre-check for existing mobile number
+        Optional<User> existingUserByMobileNo = authRepository.findByMobileNo(request.getMobileNo());
+        if(existingUserByMobileNo.isPresent()){
+            Map<String, String> errors = new HashMap<>();
+            errors.put("mobile_no", "User with provided phone number is already exists");
+            throw new SignUpDatabaseConstrainViolationException("Sign-up database constraint violation", errors);
+        }
+
+        // Save user
         authRepository.save(new User(request.getUsername(), request.getPassword(), request.getMobileNo()));
-        return Response.responseBuilder()
+        return ResponseDTO.responseBuilder()
                 .statusMessage("Sign up successfully.")
+                .data(null)
                 .build();
     }
 
-    public ResponseData<SignInResponse> signIn(SignInRequest request){
-        return ResponseData.<SignInResponse>responseDataBuilder()
+    public ResponseDTO<SignInResponse> signIn(SignInRequest request) {
+
+        //Find user
+        Optional<User> optionalUser = authRepository.findByUsername(request.getUsername());
+        if (optionalUser.isEmpty()) {
+            Map<String, String> errors = new HashMap<>();
+            errors.put("username", "User with provided username is not exist.");
+            throw new InvalidSignInDataException("Sign in failed", errors);
+        }
+        User user = optionalUser.get();
+
+        //Check password
+        if(!user.getPassword().equals(request.getPassword())){
+            Map<String, String> errors = new HashMap<>();
+            errors.put("password", "Invalid password.");
+            throw new InvalidSignInDataException("Sign in failed", errors);
+        }
+        String token = createToken(user.getUserId());
+        SignInResponse response = new SignInResponse(token);
+
+        return ResponseDTO.<SignInResponse>responseBuilder()
                 .statusMessage("Sign in successfully.")
-                .data(new SignInResponse())
+                .data(response)
                 .build();
-    }
+        }
 
-    public Response changePassword(ChangePasswordRequest request) {
-        return Response.responseBuilder()
+    public ResponseDTO<?> changePassword(ChangePasswordRequest request) {
+
+        JwtUtils.verify();
+
+        // Get user by userId inside JWT token
+        // Modify this part to add Administration Role
+        Optional<User> optionalUser = authRepository.findById(getUserId());// Not sure
+
+        // Cannot find user
+        if(optionalUser.isEmpty()){
+            Map<String, String> errors = new HashMap<>();
+            errors.put("userId", "Cannot find the user with provided userId.");
+            throw new ChangePasswordErrorException("Changing password failed. ",errors);
+        }
+        User user = optionalUser.get();
+
+        // Old password does not match
+        if(!user.getPassword().equals(request.getOldPassword())){
+            Map<String, String> errors = new HashMap<>();
+            errors.put("oldPassword", "Invalid previous password.");
+            throw new ChangePasswordErrorException("Changing password failed.",errors);
+        }
+
+        // Update new password
+        authRepository.updatePassword(user.getUserId(),request.getNewPassword());
+
+        return ResponseDTO.responseBuilder()
                 .statusMessage("Changing password successfully.")
                 .build();
     }
-
 }
