d447ea9e0d00d70f08a1a3529223699abc5a46a5
Kantawit Chatdamrongmongkol <ka1n11@hotmail.com>
2024-08-03 19:27:10 +0700

    feat(common): add global exception handler

---
+++ src/main/java/com/triptogether/api/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.triptogether.api.common.exception;
+
+import com.triptogether.api.auth.constant.StatusCode;
+import com.triptogether.api.common.dto.ResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ResponseDTO<?>> handleUserNotFoundException(UserNotFoundException ex){
+        ResponseDTO<?> response =  ResponseDTO.responseBuilder()
+                .statusCode(StatusCode.SIGN_UP_ERROR_1)
+                .statusMessage(ex.getMessage())
+                .data(null)
+                .errors(ex.getErrors())
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UpdateUserProfileErrorException.class)
+    public ResponseEntity<ResponseDTO<?>> handleUpdateUserProfileErrorException(UpdateUserProfileErrorException ex){
+        ResponseDTO<?> response =  ResponseDTO.responseBuilder()
+                .statusCode(StatusCode.SIGN_UP_ERROR_1)
+                .statusMessage(ex.getMessage())
+                .data(null)
+                .errors(ex.getErrors())
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
