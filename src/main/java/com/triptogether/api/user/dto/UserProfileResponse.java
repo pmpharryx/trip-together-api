9841da0ba6bfffe1f7d2666fafbc2a869020e8e0
Kantawit Chatdamrongmongkol <ka1n11@hotmail.com>
2024-08-03 19:58:36 +0700

    feat(user): add user profile response dto

---
+++ src/main/java/com/triptogether/api/user/dto/UserProfileResponse.java
@@ -0,0 +1,40 @@
+package com.triptogether.api.user.dto;
+
+import com.triptogether.api.common.model.User;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+public class UserProfileResponse {
+
+    private UUID userId;
+
+    private String username;
+
+    private String mobileNo;
+
+    private String createdBy;
+
+    private LocalDateTime createdAt;
+
+    private String updatedBy;
+
+    private LocalDateTime updatedAt;
+
+    public static UserProfileResponse fromUser (User user) {
+
+        UserProfileResponse userProfile = new UserProfileResponse();
+        userProfile.setUserId(user.getUserId());
+        userProfile.setUsername(user.getUsername());
+        userProfile.setMobileNo(user.getMobileNo());
+        userProfile.setCreatedBy(user.getCreatedBy());
+        userProfile.setCreatedAt(user.getCreatedAt());
+        userProfile.setUpdatedBy(user.getUpdatedBy());
+        userProfile.setUpdatedAt(user.getUpdatedAt());
+
+        return userProfile;
+    }
+}
