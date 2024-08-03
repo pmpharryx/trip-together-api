3f10aed6012312005dd1d25b17a7df3e9776859c
Kantawit Chatdamrongmongkol <ka1n11@hotmail.com>
2024-08-03 20:05:37 +0700

    feat(user): add update profile & update get profile endpoints

--- src/main/java/com/triptogether/api/user/controller/UserController.java
+++ src/main/java/com/triptogether/api/user/controller/UserController.java
@@ -1,11 +1,23 @@
 package com.triptogether.api.user.controller;
 
-import com.triptogether.api.common.model.User;
-import com.triptogether.api.common.dto.ResponseData;
+import com.triptogether.api.auth.exception.MissingTokenException;
+import com.triptogether.api.auth.exception.SignInErrorException;
+import com.triptogether.api.auth.exception.SignUpErrorException;
+import com.triptogether.api.auth.utility.JwtUtils;
+import com.triptogether.api.common.dto.ResponseDTO;
+import com.triptogether.api.common.exception.UpdateUserProfileErrorException;
+import com.triptogether.api.user.dto.UpdateUserProfileRequest;
+import com.triptogether.api.user.dto.UserProfileResponse;
 import com.triptogether.api.user.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/v1/users")
@@ -14,9 +26,46 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/{userId}")
-    public ResponseEntity<ResponseData<User>> getUserProfile(@PathVariable("userId") String userId) {
-        ResponseData<User> response = userService.getUserProfile(userId);
+    @PostMapping("/get-profile/{userId}")
+    public ResponseEntity<ResponseDTO<UserProfileResponse>> getUserProfile(@PathVariable(value = "userId") String userId,
+                                                                           @RequestHeader(value = "Authorization", required = false)
+                                                                           String authorizationHeader) {
+
+        if(authorizationHeader == null || authorizationHeader.isEmpty()){
+            Map<String, String> errors = new HashMap<>();
+            errors.put("authorizationHeader", "Authorization header is missing.");
+            throw new MissingTokenException("Missing Token Exception", errors);
+        }
+
+        JwtUtils.verify();
+
+        ResponseDTO<UserProfileResponse> response = userService.getUserProfile(userId);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/update-profile")
+    public ResponseEntity<ResponseDTO<?>> updateUserProfile(@Valid @RequestBody UpdateUserProfileRequest request,
+                                                            @RequestHeader(value = "Authorization", required = false)
+                                                            String authorizationHeader,
+                                                            BindingResult bindingResult) {
+
+        if(authorizationHeader == null || authorizationHeader.isEmpty()){
+            Map<String, String> errors = new HashMap<>();
+            errors.put("authorizationHeader", "Authorization header is missing.");
+            throw new MissingTokenException("Missing Token Exception", errors);
+        }
+
+        JwtUtils.verify();
+
+        if(bindingResult.hasErrors()){
+            Map<String, String> errors = new HashMap<>();
+            for (FieldError error : bindingResult.getFieldErrors()){
+                errors.put(error.getField(),error.getDefaultMessage());
+            }
+            throw new UpdateUserProfileErrorException("Update User Profile Error Exception", errors);
+        }
+        ResponseDTO<?> response = userService.updateUserProfile(request);
+
         return ResponseEntity.ok(response);
     }
 }
